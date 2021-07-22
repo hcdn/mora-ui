5dfe7e6d61e7972dbd2ea745f5e185576d7cc5c6
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2021-07-22 17:06:15 -0300

    spacer util

---
+++ src/utils/spacer/spacerTypes.ts
@@ -0,0 +1,31 @@
+import { DefaultTheme } from 'styled-components'
+
+export type SpaceUnit = number | 'auto'
+
+export interface MarignSpacerInterface {
+  ml?: SpaceUnit
+  mr?: SpaceUnit
+  mt?: SpaceUnit
+  mb?: SpaceUnit
+  mx?: SpaceUnit
+  my?: SpaceUnit
+  m?: SpaceUnit
+}
+export interface PaddingSpacerInterface {
+  pl?: SpaceUnit
+  pr?: SpaceUnit
+  pt?: SpaceUnit
+  pb?: SpaceUnit
+  px?: SpaceUnit
+  py?: SpaceUnit
+  p?: SpaceUnit
+}
+
+export interface buildSpacerCssInterface {
+  (
+    prop: 'margin' | 'padding',
+    side: 'all' | 'top' | 'right' | 'bottom' | 'left',
+    size: SpaceUnit,
+    theme: DefaultTheme
+  ): string
+}
