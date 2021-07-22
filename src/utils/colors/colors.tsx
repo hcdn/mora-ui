46d5ea93ba625db45bd8ccbc761a528f1779774d
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2021-07-22 15:34:22 -0300

    color utils

---
+++ src/utils/colors/colors.tsx
@@ -0,0 +1,41 @@
+import { css, DefaultTheme } from 'styled-components'
+import {
+  MainColorType,
+  MainColorVariantType,
+  StepColorLevelsType,
+  StepColorType
+} from '../../theme/colors/colorsTypes'
+
+export type MainColorNameType = keyof DefaultTheme['palette']['main']
+
+export const getMainColor = (
+  color: MainColorNameType,
+  theme: DefaultTheme
+): MainColorType => {
+  return theme.palette.main[color]
+}
+
+export const cssGetMainColor = (
+  color: MainColorNameType,
+  variant?: MainColorVariantType
+) =>
+  css`
+    ${({ theme }) => getMainColor(color, theme)[variant || 'main']}
+  `
+
+export type StepColorNameType = keyof DefaultTheme['palette']['colors']
+
+export const getStepColor = (
+  color: StepColorNameType,
+  theme: DefaultTheme
+): StepColorType => {
+  return theme.palette.colors[color]
+}
+
+export const cssGetStepColor = (
+  color: StepColorNameType,
+  level?: StepColorLevelsType
+) =>
+  css`
+    ${({ theme }) => getStepColor(color, theme)[level || 'default']}
+  `
