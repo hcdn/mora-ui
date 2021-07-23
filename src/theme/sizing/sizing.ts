30cf017343b94f38b5e8bd23bcea2aada7da1099
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2021-07-23 20:47:24 -0300

    round sizing

--- src/theme/sizing/sizing.ts
+++ src/theme/sizing/sizing.ts
@@ -2,12 +2,15 @@ import { SizingType } from './sizingTypes'
 
 export const getSize = (
   size: number | string,
-  scale: number = 0.4,
+  scale: number = 0.25,
   unit: string = 'rem'
-): string => (typeof size === 'string' ? size : `${size * scale}${unit}`)
+): string =>
+  typeof size === 'string'
+    ? size
+    : `${Math.round(size * scale * 10) / 10}${unit}`
 
 export const defaultSizing: SizingType = {
-  scale: 0.4,
+  scale: 0.25,
   unit: 'rem',
   getSize
 }
