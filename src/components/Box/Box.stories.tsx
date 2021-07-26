d1c81877d8114dddca9d170b7e516a55e5a4d2c7
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2021-07-25 21:15:43 -0300

    box docs

---
+++ src/components/Box/Box.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Button from '../Button/Button'
+import { Text } from '../Text/Text'
+import { Box } from './Box'
+// import mdx from './Box.mdx'
+
+// export default {
+//   title: 'Components/Box',
+//   component: Box,
+//   parameters: {
+//     doc: {
+//       description: {
+//         component: 'The box comopnent allows you crete layouts for components.'
+//       }
+//     }
+//   }
+// }
+
+export const Index = () => (
+  <Box container containerSize='m' p={4}>
+    <Text>I'm inside a container.</Text>
+  </Box>
+)
+
+export const FlexBox = () => (
+  <Box flex space={8} justify='center' align='center'>
+    <Text>Element 1.</Text>
+    <Text>Element 2.</Text>
+    <Button>Element 3</Button>
+  </Box>
+)
+
+export const GridBox = () => (
+  <Box cols container containerSize='l'>
+    <Box span={3}>
+      <Text>3 columns</Text>
+    </Box>
+    <Box span={9} cols>
+      <Box span={6}>
+        <Text>6 columns</Text>
+      </Box>
+      <Box span={3}>
+        <Text>3 columns</Text>
+      </Box>
+    </Box>
+  </Box>
+)
