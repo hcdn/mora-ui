0de4e122c0d0fa1f9306c3ec516a26f8d8754b5b
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2021-07-28 21:03:04 -0300

    cssBox

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { cssCreateStyles, cssGetProp, getSize } from '../../utils'
 import { cssUseBackground } from '../../utils/colors/getBackground'
 import { spacerMargin, spacerPadding } from '../../utils/spacer'
@@ -10,7 +10,42 @@ import {
 } from './BoxStyles'
 import { BoxProps, BoxWrapperProps } from './BoxTypes'
 
-export const Box = styled.div.attrs<BoxProps, BoxWrapperProps>((props) => {
+export const cssBox = css<BoxWrapperProps>`
+  ${spacerMargin}
+  ${spacerPadding}
+  ${({ flex }) => flex && flexBox}
+  ${({ align }) => cssGetProp('align-items', align)}
+  ${({ justify }) => cssGetProp('justify-content', justify)}
+  ${({ space, direction = 'row', theme }) =>
+    space &&
+    `
+    & > *:not(:last-child) {
+      ${buildInnerSpace(getSize(space, theme), direction)}
+    }
+  `}
+  ${({ colCount, theme }) =>
+    colCount &&
+    `
+    display: grid;
+    grid-template-columns: repeat(${getColCount(colCount, theme)}, 1fr);
+    grid-gap: ${theme.layout.colGap + 'rem'};
+    & > * {
+      min-width: 0;
+    }
+  `}
+  ${({ span }) => !!span && `grid-column:span ${span};`}
+  ${({ container, containerSize }) =>
+    container && buildContainer({ size: containerSize || 'l', padding: true })}
+  ${({ grow }) =>
+    grow !== undefined &&
+    `
+      flex-grow: ${grow ? 1 : 0};
+    `}
+  ${cssUseBackground}
+  ${cssCreateStyles}
+`
+
+export const transformBoxProps = (props: BoxProps): BoxWrapperProps => {
   const colCount: number | boolean =
     (props.cols === true && props.span ? props.span : props.cols) || false
   const componentProps = {
@@ -18,34 +53,12 @@ export const Box = styled.div.attrs<BoxProps, BoxWrapperProps>((props) => {
     colCount
   }
   return componentProps
-})<BoxProps>`
-  ${spacerMargin}
-  ${spacerPadding}
-${({ flex }) => flex && flexBox}
-${({ align }) => cssGetProp('align-items', align)}
-${({ justify }) => cssGetProp('justify-content', justify)}
-${({ space, direction = 'row', theme }) =>
-    space &&
-    `
-    & > *:not(:last-child) {
-      ${buildInnerSpace(getSize(space, theme), direction)}
-    }
-  `}
-${({ colCount, theme }) =>
-    colCount &&
-    `
-  display: grid;
-  grid-template-columns: repeat(${getColCount(colCount, theme)}, 1fr);
-  grid-gap: ${theme.layout.colGap + 'rem'};
-`}
-${({ span }) => !!span && `grid-column:span ${span};`}
-${({ container, containerSize }) =>
-    container && buildContainer({ size: containerSize || 'l', padding: true })}
-${({ grow }) =>
-    grow !== undefined &&
-    `
-    flex-grow: ${grow ? 1 : 0};
-  `}
-${cssUseBackground}
-${cssCreateStyles}
+}
+
+/** boxProps (no tiene col count) => transformBoxProps(boxProps) => boxWrapperProps (tiene col count) */
+
+export const Box = styled.div.attrs<BoxProps, BoxWrapperProps>(
+  transformBoxProps
+)<BoxProps>`
+  ${cssBox}
 `
